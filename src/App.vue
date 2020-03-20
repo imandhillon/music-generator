d637a695411222dafa517d07ea50906ceae41369
Iman <imansingh96@gmail.com>
2020-03-20 01:13:44 -0400

    Refactored

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,4 @@
 <template>
-  <!-- <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-
-    <v-content>
-      <SimpleUpload/>
-    </v-content>
-
-  </v-app> -->
 
   <div id="app">
     <v-app>
@@ -47,27 +25,9 @@ export default {
     SimpleUpload,
   },
 
-  // methods: {
-
-  // },
-  
-  // computed: {
-  //   isDisabled() {
-  //     const fileUploaded = this.canGen
-
-  //     console.log('comp')
-
-
-  //     return fileUploaded
-  //   }
-  // },
 
   data: () => ({
-    // file: "",
-    // msg: "",
-    // error: false,
-    // uploading: false,
-    // canGen: false
+
   })
 };
 </script>
